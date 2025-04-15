d3aa537af26c1185b0726c6ed18ddd2c7ca66792
Joël Poyet <joel.poyet1@swisscom.com>
2025-04-15 17:46:11 +0200

    fix: disable mobile guide

--- src/toasts/MobileGuideToast.ts
+++ src/toasts/MobileGuideToast.ts
@@ -12,7 +12,7 @@ import ToastStore from "../stores/ToastStore";
 import SdkConfig from "../SdkConfig";
 
 const onAccept = (): void => {
-    window.location.href = "mobile_guide/";
+    window.location.href = "https://healthchat.ch";
 };
 
 const onReject = (): void => {
